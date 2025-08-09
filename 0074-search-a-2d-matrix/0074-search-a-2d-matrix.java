3c307a8eb180a6d4ce35a9985da8f7b52d70e195
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2025-08-09 08:18:32 +0530

    0074-SEARCH-A-2D-MATRIX Stats: Time: 0 ms (100%), Space: 42.5 MB (18.04%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,12 +1,10 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        ArrayList<Integer> arr = new ArrayList<>();
         for(int i=0;i<matrix.length;i++){
             for(int j =0;j<matrix[i].length;j++){
-                arr.add(matrix[i][j]);
+                if(matrix[i][j] == target) return true;
             }
         }
-        System.out.println(arr);
-        return (arr.contains(target))? true:false;
+        return false;        
     }
 }
