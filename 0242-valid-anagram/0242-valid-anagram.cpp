aa1d1446a7e1112701118f72e02d3edba658aa62
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2025-09-21 09:03:05 +0530

    0242-VALID-ANAGRAM Stats: Time: 7 ms (20.28%), Space: 9.7 MB (48.43%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        map<char,int> freq;
+        for (auto i: s){
+            freq[i]++;
+        }
+        for(auto it : t){
+            if(freq.find(it) == freq.end() || freq[it] == 0) return false;
+            freq[it]--;
+        }
+        return true;
+    }
+};
