8efb177015098aaccbee49b951b6c81809445c38
Pranav Titambe <141265654+Pranavlovescode@users.noreply.github.com>
2025-12-25 16:22:33 +0530

    Time: 1 ms (12.63%), Space: 43.9 MB (13.36%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int n = nums.length;
+
+        ArrayList<Integer> sumLeft = new ArrayList<>(n);
+        ArrayList<Integer> sumRight = new ArrayList<>(n);
+
+        // Fill lists with 0s
+        for (int i = 0; i < n; i++) {
+            sumLeft.add(0);
+            sumRight.add(0);
+        }
+
+        // Prefix sum
+        sumLeft.set(0, nums[0]);
+        for (int i = 1; i < n; i++) {
+            sumLeft.set(i, sumLeft.get(i - 1) + nums[i]);
+        }
+
+        // Suffix sum
+        sumRight.set(n - 1, nums[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            sumRight.set(i, sumRight.get(i + 1) + nums[i]);
+        }
+
+        // Pivot check
+        for (int i = 0; i < n; i++) {
+            int left = (i == 0) ? 0 : sumLeft.get(i - 1);
+            int right = (i == n - 1) ? 0 : sumRight.get(i + 1);
+            if (left == right) return i;
+        }
+
+        return -1;
+    }
+}
